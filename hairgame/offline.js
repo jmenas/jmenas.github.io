393a8b303239692afa5d3766eec4bbf1641e035c
jmenas <joshuamenas@gmail.com>
2018-11-28 20:21:12 -0500

    update

--- hairgame/offline.js
+++ hairgame/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1543451806,
+	"version": 1543454448,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -9,6 +9,13 @@
 		"images/button_test-sheet0.png",
 		"images/salonbg2-default-000.jpg",
 		"images/salonbg3-default-000.jpg",
+		"images/juliabubble-sheet0.png",
+		"images/juliabubble-sheet1.png",
+		"images/juliabubble-sheet2.png",
+		"images/juliabubble-sheet3.png",
+		"images/juliabubble-sheet4.png",
+		"images/juliabubble-sheet5.png",
+		"images/juliabubble-sheet6.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
